1fac2ef5a8b2e8cf1ed0cbc89039397ced52eccc
Ahsan <50600624+atebha@users.noreply.github.com>
2025-09-07 14:23:38 -0700

    test run of qr code using web urls

--- main.py
+++ main.py
@@ -6,3 +6,15 @@ segno
 
 # Import Libraries
 import os
+import segno
+
+# start of program
+website = input("Enter the website URL: ")
+file_name = input("Enter the name you want to save the QR code as (without extension): ")
+print(f"The QR Code will save into the current directory as a png file type.")
+
+# Generate QR code
+qr = segno.make(website)
+
+# Save QR code
+qr.save("qr.png")
